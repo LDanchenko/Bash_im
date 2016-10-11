5cb9bb3d95a8d5a1baf0979a34848774596592f2
user <2l-u-b-o-v@ukr.net>
2016-10-11 12:52:14 +0300

    Добавлен Toolbar

--- app/src/main/java/com/ldv/bash_im/MainActivity.java
+++ app/src/main/java/com/ldv/bash_im/MainActivity.java
@@ -1,10 +1,32 @@
 package com.ldv.bash_im;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    @ViewById
+    Toolbar toolbar;
+
+
+      @AfterViews
+      void ready(){
+          toolbar.setTitle("TOOLBAR");
+          setupActionBar();
+      }
+
+    private void setupActionBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
 }
