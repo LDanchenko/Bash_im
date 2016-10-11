a2f6426a9342e7cb8e599d11e58f87f6a769b3f0
user <2l-u-b-o-v@ukr.net>
2016-10-11 13:12:04 +0300

    Добавлен NavigationDrawer

--- app/src/main/java/com/ldv/bash_im/MainActivity.java
+++ app/src/main/java/com/ldv/bash_im/MainActivity.java
@@ -1,32 +1,63 @@
 package com.ldv.bash_im;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import static android.R.id.toggle;
+
 @EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @ViewById
     Toolbar toolbar;
 
+    @ViewById (R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @ViewById(R.id.navigation_view)
+    NavigationView navigationView;
 
       @AfterViews
       void ready(){
           toolbar.setTitle("TOOLBAR");
           setupActionBar();
+          setupDrawerLayout();
       }
 
+    //добавление тулбара
     private void setupActionBar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {actionBar.setDisplayHomeAsUpEnabled(true);
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
+        //добавление навигации
+        private void setupDrawerLayout() {
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                    R.string.navigation_drawer_close);
+            drawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
+            navigationView.setNavigationItemSelectedListener(this);
+            setTitle(getString(R.string.app_name));
+        }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }
+
+
