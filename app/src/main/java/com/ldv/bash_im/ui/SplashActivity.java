cf8f437109fb7abca8ad83f0e8d2169ee718d0bf
user <2l-u-b-o-v@ukr.net>
2016-10-17 23:42:47 +0300

    Добавлена транзакция

--- app/src/main/java/com/ldv/bash_im/ui/SplashActivity.java
+++ app/src/main/java/com/ldv/bash_im/ui/SplashActivity.java
@@ -11,6 +11,9 @@ import com.ldv.bash_im.R;
 import com.ldv.bash_im.rest.NetworkStatusChecker;
 import com.ldv.bash_im.rest.StoriesModel;
 import com.ldv.bash_im.ui.entities.StoriesEntity;
+import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
+import com.raizlabs.android.dbflow.structure.database.transaction.ITransaction;
 
 
 import org.androidannotations.annotations.AfterViews;
@@ -51,36 +54,39 @@ public class SplashActivity extends AppCompatActivity {
     }
 
 
-   public void updateDB(List<StoriesModel> storiesEntities) {
+   public void updateDB(final List<StoriesModel> storiesEntities) {
+       FlowManager.getDatabase(StoriesDatabase.class).executeTransaction(new ITransaction() {
+           @Override
+           public void execute(DatabaseWrapper databaseWrapper) {
+               if (StoriesEntity.selectAll().isEmpty()) {
+                   for (StoriesModel stories : storiesEntities) {
+                       StoriesEntity story = new StoriesEntity(stories.getName(), stories.getSite(),
+                               stories.getDesc(), stories.getLink(), stories.getElementPureHtml(), false);
+                       story.save();
+                   }
+               } else if (StoriesEntity.selectAll().size() != 0) {
+                   for (StoriesModel stories : storiesEntities) {
+                       String link = stories.getLink();
+                       StoriesEntity tr = StoriesEntity.selectBylink(link);
 
-      if (StoriesEntity.selectAll().isEmpty()){
-           for (StoriesModel stories : storiesEntities){
-               StoriesEntity story = new StoriesEntity(stories.getName(), stories.getSite(),
-                       stories.getDesc(), stories.getLink(), stories.getElementPureHtml(), false);
-               story.save();
+
+                       try { //если нашли с такой ссылкой обьект в базе, просто переписываем ему все кроме favorite
+                           tr.setName(stories.getName());
+                           tr.setSite(stories.getSite());
+                           tr.setDesc(stories.getDesc());
+                           tr.setLink(stories.getLink());
+                           tr.setElementPureHtml(stories.getElementPureHtml());
+                           tr.save();
+                       } catch (NullPointerException e) { //если такого обьекта нет, создаем новый
+                           StoriesEntity storiesEntity = new StoriesEntity(stories.getName(), stories.getSite(),
+                                   stories.getDesc(), stories.getLink(), stories.getElementPureHtml(), false);
+                           storiesEntity.save();
+                       }
+                   }
+
+               }
            }
-       }
-       else if (StoriesEntity.selectAll().size()!=0) {
-          for (StoriesModel stories : storiesEntities) {
-              String link = stories.getLink();
-              StoriesEntity tr = StoriesEntity.selectBylink(link);
-
-
-              try { //если нашли с такой ссылкой обьект в базе, просто переписываем ему все кроме favorite
-                    tr.setName(stories.getName());
-                    tr.setSite(stories.getSite());
-                    tr.setDesc(stories.getDesc());
-                    tr.setLink(stories.getLink());
-                    tr.setElementPureHtml(stories.getElementPureHtml());
-                    tr.save();
-                }
-              catch (NullPointerException e){ //если такого обьекта нет, создаем новый
-                  StoriesEntity storiesEntity = new StoriesEntity(stories.getName(), stories.getSite(),
-                          stories.getDesc(), stories.getLink(), stories.getElementPureHtml(), false);
-                  storiesEntity.save(); }
-              }
-
-      }
+       });
    }
 
 
